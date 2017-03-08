d4128b1f637449ed6da43b4245840dffa362c68f
Janak Soni <jakzi7005@gmail.com>
2017-03-08 00:42:38 -0800

    finished

--- duplicate-search.js
+++ duplicate-search.js
@@ -3,34 +3,31 @@ const
   dir = require('node-dir')
 treeify = require('treeify')
 crypto = require('crypto');
-const hash = crypto.createHash('sha256')
 let directory = process.argv.slice(2).toString()
-console.log(directory)
-let files = {}
+let data = {}
 dir.readFiles(directory,
-  function(err, content, filepath, next) {
+  function(err, content, files, next) {
     if (err)
       throw err;
-    contents = content.trim().split('\r\n')
+    contents = content.trim().split('\r\n').toString()
     const hash = crypto.createHash('sha256')
-    contents.forEach((item) => {
-      hash.update(item)
-      const hashValue = hash.digest('hex').toString()
-      const fileloc = filepath.toString()
-      files[fileloc] = hashValue
-      const key = Object.keys(files)
-      const value = Object.values(files)
-      for (var i = 0; i < value.length; i++) {
-        for (var j = i + 1; j < value.length; j++) {
-          if (value[j] === value[i] && i !== j) {
-            console.log(treeify.asTree({
-              [key[i]]: {
-                [key[j]]: null
-              }
-            }))
-          }
-        }
-      }
-    })
+    hash.update(contents)
+    data[files] = hash.digest('hex')
     next()
+  },
+  function(err, files) {        
+    if (err) throw err;        
+    const key = Object.keys(data)        
+    const value = Object.values(data)        
+    for (let i = 0; i < value.length; i++) {            
+      for (let j = i + 1; j < value.length; j++) {                
+        if (value[j] == value[i] && i !== j) {                    
+          console.log(treeify.asTree({
+            [key[i]]: {
+              [key[j]]: null
+            }
+          }))                
+        }            
+      }        
+    }    
   })
